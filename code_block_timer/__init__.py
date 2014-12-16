5c83d17ec0bb09cf80ce6d92c9d4355ce6978508
John Eskew <jeskew@edx.org>
2014-12-16 16:54:38 -0500

    Fix module-wide nest_level var

--- code_block_timer/__init__.py
+++ code_block_timer/__init__.py
@@ -3,7 +3,12 @@ from timeit import default_timer
 
 from .storage import TimingDataStorage
 
-nest_level = 0
+class Globals(object):
+    pass
+
+__m = Globals()
+__m.nest_level = 0
+
 
 class CodeBlockTimer(object):
     def __init__(self, block_desc, verbose=False):
@@ -14,9 +19,9 @@ class CodeBlockTimer(object):
         self.run_id = None
 
     def __enter__(self):
-        if nest_level == 0:
+        if __m.nest_level == 0:
             self.run_id = self.data_store.run_id()
-        nest_level += 1
+        __m.nest_level += 1
         self.start = self.timer()
         return self
 
@@ -25,8 +30,8 @@ class CodeBlockTimer(object):
         self.elapsed_secs = end - self.start
         self.elapsed = self.elapsed_secs * 1000  # millisecs
         self.data_store.store(self.run_id, self.block_desc, self.elapsed)
-        nest_level -= 1
-        if nest_level == 0:
+        __m.nest_level -= 1
+        if __m.nest_level == 0:
             self.run_id = None
         if self.verbose:
             print '{}: elapsed time: {} ms'.format(self.block_desc, self.elapsed)
